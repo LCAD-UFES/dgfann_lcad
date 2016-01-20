2a5c0171ced4f1d588f175622cc512376ded9e7d
Jacson Rodrigues Correia da Silva <jacsonrcsilva@gmail.com>
2016-01-20 19:03:19 -0200

    update

--- dgfann_aoc/utils/02-gen_plots.sh
+++ dgfann_aoc/utils/02-gen_plots.sh
@@ -26,14 +26,11 @@ for i in $DIR_RNAS/*;do
     N=`basename ${i}`
     N=${N%%-*}
     ./$TESTE_RNA $i $ARQ_TESTES > $DIR_OUTPUTS/teste_RNA-$N.txt
-    for PED in 1 4 8;do
-        ./utils/03-org_graph.py $PED $DIR_OUTPUTS/teste_RNA-${N}.txt $DIR_OUTPUTS/teste_RNA-${N}_${PED}.txt
-        MSE=$(awk '{if ($5 == "Mean") print $NF}' $DIR_OUTPUTS/teste_RNA-${N}.txt)
-        ARQ="$DIR_OUTPUTS/teste_RNA-${N}_${PED}.txt"
-        echo "set term pngcairo font 'Times New Roman,10';
-              set output '${DIR_PLOTS}/figura_${N}_${PED}.png';
-              set title 'ANN $N (MSE: $MSE)';
-              plot '$ARQ' using 2 title 'Estimated' with lines, '$ARQ' using 4 title 'Desired' with lines, '$ARQ' using 6 title 'Error' with lines" | gnuplot
-     done
-
+    ./utils/03-org_graph.py 1 $DIR_OUTPUTS/teste_RNA-${N}.txt $DIR_OUTPUTS/teste_RNA-${N}_1.txt
+    MSE=$(awk '{if ($5 == "Mean") print $NF}' $DIR_OUTPUTS/teste_RNA-${N}.txt)
+    ARQ="$DIR_OUTPUTS/teste_RNA-${N}_1.txt"
+    echo "set term pngcairo font 'Times New Roman,10';
+          set output '${DIR_PLOTS}/figura_${N}.png';
+          set title 'ANN $N (MSE: $MSE)';
+          plot '$ARQ' using 2 title 'Estimated' with lines, '$ARQ' using 4 title 'Desired' with lines, '$ARQ' using 6 title 'Error' with lines" | gnuplot
 done
