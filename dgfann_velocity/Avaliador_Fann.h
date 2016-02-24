a921f23eaffab7f956603ace585bda78faffbb31
Jacson Rodrigues Correia da Silva <jacsonrcsilva@gmail.com>
2016-02-24 15:50:50 -0300

    Not necessary

--- dgfann_velocity/Avaliador_Fann.h
+++
@@ -1,7 +0,0 @@
-
-#define dirSaveRNAs      "Redes_Geradas"
-#define nomeArqTreino    "entradas/treino.train"
-#define nomeArqValidacao "entradas/validacao.train"
-#define qntNeuroniosEntrada 360
-#define qntNeuroniosSaida   1
-#define desiredError        0.001500
