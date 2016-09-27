a5bf499c105cdd01469a51e680b98a27cb44ac43
Jacson Rodrigues <jacsonrcsilva@gmail.com>
2016-09-27 10:39:34 -0300

    Update Ordenar_Dataset.py

--- dgfann_aoc/utils/Ordenar_Dataset.py
+++ dgfann_aoc/utils/Ordenar_Dataset.py
@@ -8,16 +8,21 @@ if len(argv) <> 4:
     print "Use: %s arqDoDataset arqComTodosPadroes arqSaida" % argv[0]
     exit(1)
 
-files = {
-         'dataset'  : argv[1],
-         'fulldata' : argv[2],
-         'output'   : argv[3]
-        }
+def printF(msg, newLine=True):
+    if newLine: print msg
+    else:       print msg,
+    stdout.flush()
 
 def checkFiles(f):
     if not access(f, R_OK):
         print "ERROR: It was impossible to access %s" % f
         exit(2)
+
+files = {
+         'dataset'  : argv[1],
+         'fulldata' : argv[2],
+         'output'   : argv[3]
+        }
 
 checkFiles( files['fulldata'] )
 checkFiles( files['dataset']  )
@@ -28,20 +33,14 @@ fulldata   = open(files['fulldata']).readlines()[1:]
 dataset    = open(files['dataset'] ).readlines()
 header     = dataset[0]
 dataset    = dataset[1:]
-# sorted dataset
-sortedData = {}
 
-print 'Sorting...'
-size  = len(dataset)
-count = 1.0
-steps = size / 100
-for p in dataset:
-    if count % steps == 0:
-        print '\b'*10, '%d%%' % int((count/size)*100),
-        stdout.flush()
-    sortedData[fulldata.index(p)] = p
-    count+=1
-print '\b'*10,'100%\nok'
+printF('Sorting...', False)
+dataset.sort(key=fulldata.index)
+printF('ok')
 
-open(files['output'], 'w').write(header);
-open(files['output'], 'a').writelines(sortedData.values())
+printF('Saving...', False)
+o = open(files['output'], 'w')
+o.write( header )
+o.writelines( dataset )
+o.close()
+printF('ok\nDone.')
